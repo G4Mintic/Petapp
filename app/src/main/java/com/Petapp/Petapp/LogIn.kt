ccba9bd62d34d891ed6206df9d03e7ca1a6daf7f
AlejaCelemin <alejaceleminmartinez@gmail.com>
2021-12-01 20:14:41 -0500

    Vista del logo

---
+++ app/src/main/java/com/Petapp/Petapp/LogIn.kt
@@ -0,0 +1,11 @@
+package com.Petapp.Petapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
