53c3bfd62e6052789331c9f5b48d88ecf7115d7f
AlejaCelemin <alejaceleminmartinez@gmail.com>
2021-12-02 19:12:59 -0500

    Vista del logo

--- app/src/main/java/com/Petapp/Petapp/LogIn.kt
+++ app/src/main/java/com/Petapp/Petapp/LogIn.kt
@@ -1,11 +1,20 @@
 package com.Petapp.Petapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        buttonRegistro.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+
+        }
+
+
     }
 }
